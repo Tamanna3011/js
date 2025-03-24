56891a2847f4b5fa2c03b776d2edb8539c206885
Tamanna3011 <154615798+Tamanna3011@users.noreply.github.com>
2025-03-24 18:08:06 +0000

    objects

--- 2_basics/3_objects.js
+++ 2_basics/3_objects.js
@@ -1,1 +1,38 @@
-//
+// There are two ways to declare objects 
+// 1) Constructor 2)literal
+// ** singleton( An object that can only be instantiated one time) :- when objects declared through literal then singleton is not created
+// But if declared through a constructor then singleton is always created
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Tamanna",
+    "full name": "Tamanna Parween",
+    [mySym]: "mykey1", // correct syntax to use a symbol
+    age: 21,
+    location: "Ranchi",
+    email: "tamanna@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+// console.log(typeof JsUser.mySym);
+
+JsUser.email = "tamanna@yahoo.com"
+//Object.freeze(JsUser)
+JsUser.email = "tamanna@microsoft.com"
+// console.log(JsUser);
+
+// in js functions can be treated like variables . In functions there is no discrimination
+JsUser.greeting = function(){
+    console.log("Helo JS user");
+    
+}
+
+console.log(JsUser.greeting);
