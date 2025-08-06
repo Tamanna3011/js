066aac5da879fefaa811afe22566eb9fafcc6154
Tamanna3011 <154615798+Tamanna3011@users.noreply.github.com>
2025-08-06 18:12:56 +0000

    scope

--- 3_basics/2_scopes.js
+++ 3_basics/2_scopes.js
@@ -13,19 +13,38 @@ if(true){          //this whole
 // console.log(b);
 // console.log(c);
 
-
+/**nested scope */
 
 function one(){
     const username = "tamanna"
 
     function two(){
         const website = "youtube"
-        console.log(username);
+        // console.log(username);
     }
-    //console.log(website);
+    //console.log(website); It will give error because the 
+                           // 'website' is defined inside the two() and it is called 
+                           // outside the scope of function two()                     
 
-    two()
+    two() // It will give the result 'tamanna' as when two() executed
+          // it goes in line 23 to console username and the username 
+          // is defined in function one() as one() is executed
+          // But if we comment this two() it will return nothing
     
 }
 
 one()
+
+if (true) {
+    const username = "tamanna"
+    if (username === "tamanna") {
+        const website = "youtube"
+        //console.log(username + website);   //this will give 'tamannayoutube' as a result
+    }
+    //console.log(website); // this will give error because it is called 
+                          // outside the 2nd scope where the 'website is defined'.hence not 
+                          //defined in the 1st scope
+}
+
+// console.log(username)     // This will also give error as we have console this
+                          // username outside the scope . Hence not defined.
