598e045e0b1d612ab8640ff820e03813eb124583
Tamanna3011 <154615798+Tamanna3011@users.noreply.github.com>
2025-08-06 17:08:23 +0000

    scope

--- 1_basics/7_datesInJS.js
+++ 1_basics/7_datesInJS.js
@@ -1,12 +1,12 @@
 // 
 
 let myDate = new Date()
-/*
+
 console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toLocaleString());
 console.log(typeof myDate);
-*/
+
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 2, 3)
@@ -20,10 +20,10 @@ let myTimeStamp = Date.now()
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
-//  `${newDate.getDay()} and the time`
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+// //  `${newDate.getDay()} and the time`
 
 
 newDate.toLocaleString('default', {
--- 3_basics/2_scopes.js
+++ 3_basics/2_scopes.js
@@ -1,1 +1,31 @@
-let 
+// var c = 300 // outside the the block scope is reffered as global scope
+let a = 300 // to avoid var problems related to scope ..we should use let
+
+if(true){          //this whole
+    let a = 10     // if condition here 
+    const b = 20   //is referred as 
+    //var c = 30     //block scope
+    // console.log("INNER:", a);
+    
+}
+    
+//  console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+function one(){
+    const username = "tamanna"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+    
+}
+
+one()
